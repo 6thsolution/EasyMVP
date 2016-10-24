56f2526de4fe191f06422f569b3373a293d1ecb2
Saeed Masoumi <s-masoumi@live.com>
2016-10-24 16:22:09 +0330

    Fix typo

--- .script/ci.sh
+++ .script/ci.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 cd ..
+# Generate weaver processor
 ./gradlew clean easymvp-weaver:jar
+# Manipulate java classes
 ./gradlew transformClassesWithWeaverForDebug --info
+# Run test
 ./gradlew connectedAndroidTest -PdisablePreDex -PwithDexcount -Dscan --info
