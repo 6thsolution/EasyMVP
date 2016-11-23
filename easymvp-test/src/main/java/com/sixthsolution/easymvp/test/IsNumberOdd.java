14aad7046415f3d32aee80eb7cbfe5af8e4deb76
Saeed Masoumi <s-masoumi@live.com>
2016-11-23 23:29:18 +0330

    Add use case test

---
+++ easymvp-test/src/main/java/com/sixthsolution/easymvp/test/IsNumberOdd.java
@@ -0,0 +1,30 @@
+package com.sixthsolution.easymvp.test;
+
+
+import easymvp.executer.PostExecutionThread;
+import easymvp.executer.UseCaseExecutor;
+import easymvp.usecase.ObservableUseCase;
+import rx.Observable;
+import rx.functions.Func0;
+
+/**
+ * @author Saeed Masoumi (saeed@6thsolution.com)
+ */
+
+public class IsNumberOdd extends ObservableUseCase<Boolean, Integer> {
+
+    public IsNumberOdd(UseCaseExecutor useCaseExecutor,
+                       PostExecutionThread postExecutionThread) {
+        super(useCaseExecutor, postExecutionThread);
+    }
+
+    @Override
+    protected Observable<Boolean> interact(final Integer number) {
+        return Observable.defer(new Func0<Observable<Boolean>>() {
+            @Override
+            public Observable<Boolean> call() {
+                return Observable.just(number % 2 != 0);
+            }
+        });
+    }
+}
---
+++ easymvp-test/src/test/java/com/sixthsolution/easymvp/test/usecase/UseCaseTest.java
@@ -0,0 +1,48 @@
+package com.sixthsolution.easymvp.test.usecase;
+
+import com.sixthsolution.easymvp.test.IsNumberOdd;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import easymvp.executer.PostExecutionThread;
+import easymvp.executer.UseCaseExecutor;
+import rx.Scheduler;
+import rx.observers.TestSubscriber;
+import rx.schedulers.Schedulers;
+
+/**
+ * @author Saeed Masoumi (saeed@6thsolution.com)
+ */
+
+public class UseCaseTest {
+
+
+    private UseCaseExecutor useCaseExecutor;
+    private PostExecutionThread postExecutionThread;
+
+    @Before
+    public void init() {
+        useCaseExecutor = new UseCaseExecutor() {
+            @Override
+            public Scheduler getScheduler() {
+                return Schedulers.immediate();
+            }
+        };
+        postExecutionThread = new PostExecutionThread() {
+            @Override
+            public Scheduler getScheduler() {
+                return Schedulers.immediate();
+            }
+        };
+    }
+
+    @Test
+    public void test_use_case_execution() {
+        TestSubscriber<Boolean> subscriber = new TestSubscriber<>();
+        IsNumberOdd isNumberOdd = new IsNumberOdd(useCaseExecutor, postExecutionThread);
+        isNumberOdd.execute(10).subscribe(subscriber);
+        subscriber.assertValue(false);
+        subscriber.assertCompleted();
+    }
+}
