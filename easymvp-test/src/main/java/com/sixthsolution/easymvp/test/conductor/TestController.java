74a11185adf5a5ad9da54c3685258393ff275e18
Saeed Masoumi <s-masoumi@live.com>
2017-04-29 18:12:38 +0430

    add conductor tests

--- easymvp-test/src/main/java/com/sixthsolution/easymvp/test/conductor/TestController.java
+++ easymvp-test/src/main/java/com/sixthsolution/easymvp/test/conductor/TestController.java
@@ -10,7 +10,13 @@ import com.sixthsolution.easymvp.test.R;
 import com.sixthsolution.easymvp.test.TestPresenter;
 import com.sixthsolution.easymvp.test.View1;
 
+import easymvp.annotation.Presenter;
 import easymvp.annotation.conductor.ConductorController;
+
+import static junit.framework.Assert.assertFalse;
+import static junit.framework.Assert.assertNotNull;
+import static junit.framework.Assert.assertNull;
+import static junit.framework.Assert.assertTrue;
 
 /**
  * @author Saeed Masoumi (s-masoumi@live.com)
@@ -19,9 +25,34 @@ import easymvp.annotation.conductor.ConductorController;
 @ConductorController(presenter = TestPresenter.class)
 public class TestController extends Controller implements View1 {
 
+    @Presenter
+    TestPresenter testPresenter;
+
     @NonNull
     @Override
     protected View onCreateView(@NonNull LayoutInflater inflater, @NonNull ViewGroup container) {
         return inflater.inflate(R.layout.simple_fragment, container, false);
     }
+
+    @Override
+    protected void onAttach(@NonNull View view) {
+        super.onAttach(view);
+        assertNotNull(testPresenter);
+        assertTrue(testPresenter.isOnViewAttachedCalled());
+    }
+
+    @Override
+    protected void onDetach(@NonNull View view) {
+        super.onDetach(view);
+        assertTrue(testPresenter.isOnViewDetachedCalled());
+        assertFalse(testPresenter.isViewAttached());
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        //presenter#onDestroy will be called after this method
+        assertNotNull(testPresenter);
+    }
+
 }
